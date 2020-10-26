84475dbf2748deb73c3b6ca3bf4ec978e2587a83
Joao Leao <joaofleao@gmail.com>
2020-10-26 04:27:38 -0300

    Cliente com envio de dados, sem broadcasting

--- client/index.js
+++ client/index.js
@@ -1,10 +1,31 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
 var fs = require('fs');
 var md5 = require('md5');
 
-//TODO Peer
-// maybe https://socket.io/blog/socket-io-p2p/
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-// Hash das Files
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3333, () => {
+  console.log('listening on *:3333');
+});
+
 fs.readFile('./files/hp.txt', function(err, buf) {
   console.log(md5(buf));
 });
+
+
+
