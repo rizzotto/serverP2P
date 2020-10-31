65480aab0da01157d41c032ea89ecb57423a3766
rizzotto <guilherme.rizzotto@acad.pucrs.br>
2020-10-31 11:34:16 -0300

    setInterval 5 seconds; working healthCheck

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ app.use(express.json())
 
 app.use('/', require('./src/routes'))
 
+
+
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
--- server/src/controllers/PeerController.js
+++ server/src/controllers/PeerController.js
@@ -1,24 +1,24 @@
 let peers = []
-let peersOverlay = []
 
-function validateRestCall() {
+// healthCheck
+setInterval(function() {
   const currentDate = new Date()
 
-  // TODO - chamar essa função em uma Thread
-  peersOverlay.map(peer => {
+  peers.map(peer => {
     if((currentDate.getTime() - peer.time) / 1000 > 5) {
-      const index = peersOverlay.indexOf(peer)
-      peersOverlay.splice(index, 1)
+      const index = peers.indexOf(peer)
+      peers.splice(index, 1)
     }
   })
-}
+}, 5000);
+
 
 module.exports = {
 
     async resources(req, res) {
         res.send(peers)
     },
-
+    
     async resource(req, res) {
         const { id } = req.body
         const specificFile = peers.map(peer => {
@@ -30,10 +30,13 @@ module.exports = {
     async post(req, res) {
       const path = req.get('host').split(':')
       const { files } = req.body
+      const time = new Date()
+
       peers.push({
         files,
         ip: path[0],
         port: path[1],
+        time: time.getTime()
       })
       
       const peer = peers.filter(peer => {
@@ -46,19 +49,10 @@ module.exports = {
       const path = req.get('host').split(':')
       const time = new Date()
   
-      const found = peersOverlay.find(element => element.port === path[1]);
-      
-      if(found !== undefined) {
-        peersOverlay.map(peer => {
-          // TODO - colocar todo o objeto
-          if(peer.port === path[1]) peer.time = time.getTime()
-        })
-      } else {
-        peersOverlay.push({
-          ip: path[0],
-          port: path[1],
-          time: time.getTime(),
-        })
-      }
+      peers.map(peer => {
+        if(peer.port === path[1]) peer.time = time.getTime()
+      })
+
+      res.send(peers)
     }
 }
