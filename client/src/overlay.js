87054c6a01fac79306044032d1ed603f533c96ba
rizzotto <guilherme.rizzotto@acad.pucrs.br>
2020-11-01 17:59:06 -0300

    refatorada

--- client/src/overlay.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios')
-
-console.log('Overlay ativado, realizando chamadas a cada 5 segundos')
-setInterval(function() {
-  console.log('chamou')
-  axios.patch('http://localhost:3333/health')  
-}, 5000);
